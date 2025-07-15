dbf7094ece517cfea1fefd8b5ac1dfa19f83eec3
lordkyrios <s506981@gmail.com>
2025-07-14 21:17:19 -0500

    Delete pages directory

--- pages/faces.tsx
+++
@@ -1,5 +0,0 @@
-// FaceAnimator frontend
-// pages/faces.tsx
-export default function FacesPage() {
-  return <div>This is the Faces page</div>;
-}
--- pages/videos.tsx
+++
@@ -1,5 +0,0 @@
-// VideoLibrary frontend
-// pages/videos.tsx
-export default function VideosPage() {
-  return <div>This is the Videos page</div>;
-}
